bfe6005c5aee817f2c8a123d4672f4b04ec127c2
terentyevkirill <kirillterentyev1999@gmail.com>
2020-08-11 13:25:27 +0200

    OddOccurrencesInArray reimplemented with O(N) complexity using XOR operator as accumulator

--- src/main/java/com/terentiev/codility/arrays/OddOccurrencesInArray.java
+++ src/main/java/com/terentiev/codility/arrays/OddOccurrencesInArray.java
@@ -1,33 +1,21 @@
 package com.terentiev.codility.arrays;
 
-
-import java.util.Arrays;
-
 public class OddOccurrencesInArray {
     public int solution(int[] A) {
-        int[] pairedIndexes = new int[A.length];
-        Arrays.fill(pairedIndexes, -1);
+        if (A.length == 0)
+            return 0;
 
-        for (int i = 0; i < A.length; i++) {
-            // looking for pair for A[i]
-            if (pairedIndexes[i] == -1) {
-                boolean isPairFound = false;
-                int pairValue = A[i];
-                int j = i + 1;
-                while (!isPairFound && j < A.length) {
-                    if (A[j++] == pairValue) {
-                        // found pair for A[i]
-                        pairedIndexes[i] = i;
-                        pairedIndexes[j - 1] = j - 1;
-                        isPairFound = true;
-                    }
-                }
-                if (!isPairFound) {
-                    // pair for A[i] not found
-                    return pairValue;
-                }
-            }
+        /*
+        Using XOR (^):
+        if A=B: A^B=0
+        A^B^C=C
+         */
+
+        int unpaired = A[0];
+        for (int i = 1; i < A.length; i++) {
+            unpaired = unpaired ^ A[i];
         }
-        return 0;
+
+        return unpaired;
     }
 }
